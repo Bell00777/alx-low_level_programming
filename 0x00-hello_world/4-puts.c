832a86a5401cc9d358efa7bdd657d4dd56d22239
my <myemail@gmail.com>
2023-02-16 12:52:06 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-
 /**
 *main-Entry point
 *
 *Description: A C program that prints with put function
 *
-*Return:Always0(Success)
+*Return:Always 0(Success)
 */
 
 int main(void)
 {
-       puts(""Programming is like building a multilingual puzzle");
+       puts("\"Programming is like building a multilingual puzzle");
        return(0);
 }
