06248d5c6c7262d5708339cbdf684488d9f06e3a
my <myemail@gmail.com>
2023-02-21 15:19:17 -0800

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,39 +10,39 @@
 
 void print_to_98(int n)
 {
-    int i;
+	int i;
 
-    if (n > 98)
-    {
-        for (i = n ; i >= 98 ; i--)
-        {
-            if (i == 98)
-            {
-                printf("%d", i);
-            }
-            else
-            {
-                printf("%d, ", i);
-            }
-        }
-    }
-    else if (n < 98)
-    {
-        for (i = n ; i <= 98 ; i++)
-        {
-            if (i == 98)
-            {
-                printf("%d", i);
-            }
-            else
-            {
-                printf("%d, ", i);
-            }
-        }
-    }
-    else if (n == 98)
-    {
-        printf("%d", n);
-    }
-    printf("\n");
+	if (n > 98)
+	{
+		for (i = n ; i >= 98 ; i--)
+		{
+			if (i == 98)
+			{
+				printf("%d", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
+		}
+	}
+	else if (n < 98)
+	{
+		for (i = n ; i <= 98 ; i++)
+		{
+			if (i == 98)
+			{
+				printf("%d", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
+		}
+	}
+	else if (n == 98)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
 }
